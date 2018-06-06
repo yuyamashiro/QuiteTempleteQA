8803d45c8c0a004402e137b75054f991e5ae95ec
yuyamashiro <yuyamashiro89@gmail.com>
2018-06-06 12:57:10 +0900

    add gap map

--- QutipTempQA/qa_statics/energygap.py
+++ QutipTempQA/qa_statics/energygap.py
@@ -79,3 +79,34 @@ def draw_min_gap(fig_path, Nlist, system, params, variables):
     plt.savefig(fig_path + 'gap' + filename_from(N, 1, params, system.params_name) + '.pdf')
 
 
+def draw_gapmap(fig_path, N, system, params, slist, newparam_list, axis_label, ticks=None):
+    all_param = params.copy()
+    gap_mat = []
+    data_path = "./data/gap_mat/gapmat_" + filename_from(N, '_', params, names=system.params_name) + ".dat"
+
+    if os.path.exists(data_path):
+        print("-- {} is exist -- will load".format(data_path))
+        gap_mat = np.loadtxt(data_path)
+    else:
+        for nparam in newparam_list[1]:
+            all_param[newparam_list[0]] = nparam
+            sys = system(T=1, N=N, param=all_param)
+            spectrum = []
+            for s in slist:
+                spectrum.append(sys.H(s).eigenstates()[0])
+            spectrum = np.array(spectrum).T
+            gap_mat.append(spectrum[1] - spectrum[0])
+        np.savetxt(data_path, gap_mat)
+
+    plot_setting(font_size=10)
+    if ticks is None:
+        plt.xticks(range(0, 110, 10), np.arange(0, 1.1, 0.1))
+        plt.yticks(range(0, 110, 10), np.arange(0, 1.1, 0.1)[::-1])
+    else:
+        plt.xticks(ticks['x'][0], ticks['x'][1])
+        plt.xticks(ticks['y'][0], ticks['y'][1])
+    plt.xlabel(axis_label[0])
+    plt.ylabel(axis_label[1])
+    plt.imshow(np.array(gap_mat).T[::-1])
+    plt.savefig(fig_path + 'gapmat_' + filename_from(N, '_', params, names=system.params_name) + ".pdf")
+
